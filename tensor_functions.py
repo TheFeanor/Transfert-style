8e14fb4afd7fa56f1eb498f7defc89d2ef7b241d
Thomas Eboli <teboli@enst.fr>
2016-12-01 20:27:35 +0100

    Computation of style error

--- tensor_functions.py
+++ tensor_functions.py
@@ -12,16 +12,6 @@ def features_distance(A, B):
     return distance
 
 
-def responses_correlation(A, B):
-    """
-    Computes de Gram matrix correlation between two filter responses A and B
-    """
-    product = A*B
-    gram = np.sum(product)
-
-    return gram
-
-
 def style_representation(A):
     """
     Computes the style representation of the feature map A along axis 3 that
@@ -33,6 +23,26 @@ def style_representation(A):
     for i in np.arange(shape_A[3]):
         for j in np.arange(shape_A[3]):
             # computes correlation between response of filter i and j
-            Gram[i,j] = responses_correlation(A[:,:,:,i], A[:,:,:,j])
+            product = A[:,:,:,i] * A[:,:,:,j]
+            Gram[i,j] = np.sum(product)
 
     return Gram
+
+
+def style_error(features_a, features_x, w = np.ones(5)/5):
+    """
+    Computes style error based on the definition of the paper.
+    w is a vector of weights of size (1,5)
+    """
+    E = 0
+
+    for k in np.arange(5):
+        N_I = features_a[k].shape[3]
+        M_I = features_a[k].shape[1] * features_a[k].shape[2]
+
+        A = style_representation(features_a[k])
+        G = style_representation(features_x[k])
+
+        E += w[k] / (4* M_I**2 * N_I**2) * norm(A-G)**2
+
+    return E
